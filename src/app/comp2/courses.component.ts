3d59bf529b07217b50471bba54fa021ca344dd32
Diego Dias <midiaon-07@Diegos-iMac.local>
2016-09-09 18:44:05 -0300

    paulo zulu

---
+++ src/app/comp2/courses.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'courses',
+    template: "<input placeholder='courses'></input>"
+})
+
+export class CoursesComponent {
+
+}
